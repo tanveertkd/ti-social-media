6a3a73ba5ac67cfb9b6c624fc12f95e32e98dc03
tanveertkd <hazarikatanveer12@gmail.com>
2022-05-17 19:25:26 +0530

    feat: added suggested users

--- src/components/SideBar/SideBarRight.jsx
+++ src/components/SideBar/SideBarRight.jsx
@@ -1,18 +1,21 @@
+import { useSelector } from "react-redux";
+
 const SideBarRight = () => {
+    const {users} = useSelector(state => state.users);
     return (
         <div className="hidden lg:block lg:w-3/5 lg:mx-auto xl:w-9/12 xl:ml-0 border-[1px] border-color-grey rounded">
             <h3 className="text-xl text-left p-2 w-full bg-color-grey text-primary-bg">
                 Who to follow?
             </h3>
             <div className="flex flex-col w-full px-2">
-                {[1, 2, 3].map((users) => {
+                {users.map((user) => {
                     return (
-                        <div className="flex justify-between py-1">
+                        <div className="flex justify-between py-1" key={user._id}>
                             <li className="sidebar-list-item text-lg list-none flex items-center hover:cursor-pointer">
                                 <i className="far fa-user-circle pr-2 text-3xl"></i>
-                                <div className="flex flex-col">
-                                    <p>John Doe</p>
-                                    <p>@doejohn</p>
+                                <div className="flex flex-col justify-start">
+                                    <p className="text-left">{user?.firstName} {user?.lastName}</p>
+                                    <p className="text-left">@{user?.username}</p>
                                 </div>
                             </li>
                             <li className="sidebar-list-item text-xl list-none flex items-center hover:cursor-pointer">
@@ -20,7 +23,7 @@ const SideBarRight = () => {
                             </li>
                         </div>
                     );
-                })}
+                }).slice(0, 3)}
             </div>
         </div>
     );
