0a9141064125dd62b52fdbf0d751388e200245df
tanveertkd <hazarikatanveer12@gmail.com>
2022-06-03 17:15:01 +0530

    docs-added comments.md

---
+++ comments.md
@@ -0,0 +1,5 @@
+### Changes
+* Changed placeholder data and some other minor changes
+* Added share feature
+* Updated users - replaced placeholder user data
+* Added debounce and modal to search
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../features/auth/authSlice';
 import { setSearchedUser } from '../../features/user/userSlice';
